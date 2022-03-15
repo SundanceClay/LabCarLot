4707b917ba183ce243364430bf27ac21b6a58c83
Shawn Penning <penningsp@hotmail.com>
2022-03-14 21:13:12 -0400

    Add project files.

---
+++ CarLotApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LabUsedCarLot
+{
+    public class CarLotApp 
+    {
+        public static void ListCars(List<Car> carList)
+        {
+            int i = 1;
+            Console.WriteLine(); // add a line space
+            foreach (Car car in carList) 
+            {
+
+                Console.WriteLine($"{i} {car.ToString()}"); 
+                i++;
+            }
+            Console.WriteLine(); // add a line space
+        }
+        public static Car AddCar(bool isNew)
+        {
+
+            Console.WriteLine("Car Adder: Enter the Make: ");
+            string make = Console.ReadLine();
+            Console.WriteLine("Car Adder: Enter the Model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Car Adder: Enter the Year: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Car Adder: Enter the Price: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            if (isNew)
+                return new Car(make, model, year, price);
+            else
+            {
+                Console.WriteLine("Car Adder: Enter the Mileage: ");
+                double mileage = double.Parse(Console.ReadLine());
+                return new UsedCar(make, model, year, price, mileage);
+
+            }
+        } 
+        
+        public static List<Car> BuyCar(int whichCar, List<Car> carList)
+        {
+            Console.WriteLine($"{carList[whichCar -1]} removed.\n");
+            carList.Remove(carList[whichCar - 1]);
+            Console.WriteLine(); // add a line space
+            return carList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using LabUsedCarLot;
+
+bool repeat = true;
+
+List<Car> carList = new List<Car>();
+var car1 = new Car("Ford", "Fusion", 2006, 20500.99m);
+var car2 = new UsedCar("Lincoln", "Mercury", 1997, 5432.11m, 200000);
+var car3 = new UsedCar("Volkswagen", "Passat", 2003, 5032.00m, 100350);
+var car4 = new Car("Mini", "Cooper", 2021, 15000m);
+var car5 = new UsedCar("Ford", "Focus", 2000, 12345.67m, 200);
+var car6 = new Car("Chrysler", "Bus", 2022, 99988.80m);
+
+carList.Add(car1);
+carList.Add(car2);
+carList.Add(car3);
+carList.Add(car4);
+carList.Add(car5);
+carList.Add(car6);
+
+string yn = "y";
+while (yn == "y")
+{
+    bool isNew;
+    Console.WriteLine("What would you like to do?\nChoose a number (1-5):" +
+        "\n1. Show list.  2. Add car.  3. Select/Buy car.  4. Modify car.  5. Exit.");
+    int userToDo = userChoice(5);
+    
+    switch (userToDo)
+    {
+        case 1:
+            CarLotApp.ListCars(carList);
+            break;
+        case 2:
+            Console.WriteLine("Add a car. Is it new? y or n");
+            if (Console.ReadLine().ToLower() == "y")
+                isNew = true;
+            else 
+                isNew = false;
+
+            carList.Add(CarLotApp.AddCar(isNew));
+
+            break;
+        case 3:
+            Console.WriteLine("Buy Car number 4 demo:");
+            CarLotApp.ListCars(carList);
+            buyCar = intParse.Console.ReadLine();
+            carList = CarLotApp.BuyCar(4, carList);
+            break;
+        case 4:
+            break;
+        case 5:
+            yn = "n";
+            break;
+        default:
+
+            break;
+    }
+}
+
+static int userChoice(int numChoices)
+{
+    bool parsedSuccessfully = false;
+    int userToDo = 1;
+    do
+    {
+        string userInput = Console.ReadLine();
+        parsedSuccessfully = int.TryParse(userInput, out userToDo);
+        if (!parsedSuccessfully || userToDo <= 0 || userToDo > numChoices)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Entry not valid.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    while (!parsedSuccessfully);
+    return userToDo;
+}
